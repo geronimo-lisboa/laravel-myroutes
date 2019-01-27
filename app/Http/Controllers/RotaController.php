bc3b8954d0410d5f002b7cec0e7121cf06c4020f
Geromin <luciano.geronimo.fnord@gmail.com>
2019-01-27 20:49:59 -0200

    Consegui traçar uma rota entre 2 endereços, o resultado está somente textual mas o dado chegou no cliente.

--- app/Http/Controllers/RotaController.php
+++ app/Http/Controllers/RotaController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client; //O módulo pra ser um cliente REST
 
 class RotaController extends Controller
 {
-    public function calculate($origem, $destino){
-        
-//        error_log("entrou?");
-//        error_log($origem);
-//
-//        error_log(strlen($destino));
-        //error_log(get_class($origem));
-        //('origem = '+$origem);
-        //error_log('destino = '+$destino);
+    public function calculate($origem, $destino)
+    {
+        try
+        {
+            error_log('1');
+            $apikey =  env('GOOGLE_API_KEY');
+            $url = 'https://maps.googleapis.com/maps/api/directions/json?origin='.$origem.'&destination='.$destino.'&key='.$apikey;
+            $client = new Client();
+            $response = $client->get($url);
+            $body = $response->getBody();
+            $contents = $body->getContents();
+            return $contents;
+        }
+        catch (\Exception $ex)
+        {
+            error_log("exception = ".$ex->getMessage()." line = ".$ex->getLine()." type = ".get_class($ex));
+        }
     }
 }
